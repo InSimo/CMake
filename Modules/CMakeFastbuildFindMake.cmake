61218ea0a0ca2d91f46e7f0bf0b4e4fd7158b5fb
lgross <loic.gross@insimo.fr>
2021-07-08 17:25:40 +0200

    [CMake Modules] ADD cmake file cmake to find the FBuild program

---
+++ Modules/CMakeFastbuildFindMake.cmake
@@ -0,0 +1,9 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+
+find_program(CMAKE_MAKE_PROGRAM
+  NAMES FBuild
+  NAMES_PER_DIR
+  DOC "Program used to build from fbuild.bff files.")
+mark_as_advanced(CMAKE_MAKE_PROGRAM)
